e907e36d063a5bbda4d63821bf29b7a818fbe233
blindly <blindly@users.noreply.github.com>
2018-05-16 16:01:52 -0400

    adding checking for valid image

--- source/public/vendor/app/js/class/reddit.js
+++ source/public/vendor/app/js/class/reddit.js
@@ -7,17 +7,18 @@ class Reddit {
     this.domId = data.domId;
 
     this.self = data.selfpost ? true : false;
+    this.image = data.image ? true : false;
   }
 
   hot() {
     const trending = new Request(this.apiUrl);
     fetch(trending)
-      .then( response => response.json() )
-      .then( response => {
+      .then(response => response.json())
+      .then(response => {
         console.debug(response);
         let myStories = [];
 
-        for (const story of response.data.children){
+        for (const story of response.data.children) {
 
           console.debug(story);
 
@@ -29,19 +30,26 @@ class Reddit {
             "content": story.data.url,
             "selftext": story.data.selftext,
             "domain": story.data.domain,
-            "favicon": generateFavicon( story.data.url ),
-            "show": true
+            "favicon": generateFavicon(story.data.url),
+            "show": true,
+            "image": fileValidation(story.data.url) ? true : false,
           };
 
           // No Self Posts!
-          if ( story.data.is_self ) {
+          if (story.data.is_self) {
             myStory.show = this.self;
+          }
+
+          if (this.image) {
+            if (! fileValidation(story.data.url)) {
+              myStory.show = false;
+            }
           }
 
           // No stories that have questions for title
           const isQuestion = myStory.title.indexOf('?') > -1;
           console.debug("Question: " + isQuestion);
-          if ( isQuestion ) {
+          if (isQuestion) {
             myStory.show = false;
           }
 
@@ -49,11 +57,11 @@ class Reddit {
           const blacklistDomains = [
             'boingboing.net',
             //'www.theverge.com'
-            ];
-            
-          const blacklisted = blacklistDomains.indexOf( myStory.domain ) != -1;
+          ];
+
+          const blacklisted = blacklistDomains.indexOf(myStory.domain) != -1;
           console.debug("Blacklisted: " + blacklisted);
-          if ( blacklisted ) {
+          if (blacklisted) {
             myStory.show = false;
           }
 
@@ -70,5 +78,5 @@ class Reddit {
         console.debug(myStories);
       });
   }
-  
+
 }
